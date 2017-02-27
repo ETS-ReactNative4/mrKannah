d6a2e48bf7b31d974a4542c782222301ff751572
Fadee Kannah <fadeekannah@gmail.com>
2017-02-26 22:49:42 -0800

    Set up theme and themable component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import muiThemeable from 'material-ui/styles/muiThemeable';
 import {Card, CardTitle, CardText} from 'material-ui/Card';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      <div className="apple">
-        <Card>
-          <CardTitle title="Card Title" />
-          <CardText>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-          </CardText>
-        </Card>
-        </div>
-      </div>
-    );
-  }
-}
+const App = (props) => (
+  <div className="App">
+    <div className="App-header" >
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2>Welcome to React</h2>
+    </div>
+    <p className="App-intro" style={{color: props.muiTheme.palette.textColor}}>
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+    <div className="apple">
+      <Card>
+        <CardTitle title="Card Title" />
+        <CardText>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+          Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+          Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+        </CardText>
+      </Card>
+    </div>
+  </div>
+);
 
-export default App;
+export default muiThemeable()(App);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import theme from './muiTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import App from './App';
 import './index.css';
 
 const MUI = () => (
-  <MuiThemeProvider>
+  <MuiThemeProvider muiTheme={getMuiTheme(theme)}>
     <App />
   </MuiThemeProvider>
 );
 
-ReactDOM.render(
-  <MUI />,
-  document.getElementById('root')
-);
+ReactDOM.render(<MUI />, document.getElementById('root'));
