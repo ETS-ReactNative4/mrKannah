2f2d371be90f82e39cdf1086b15bcb252f2eb95c
Fadee Kannah <fadeekannah@gmail.com>
2017-02-26 19:37:32 -0800

    Added Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Card, CardTitle, CardText} from 'material-ui/Card';
 
 class App extends Component {
   render() {
@@ -13,6 +14,17 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+      <div className="apple">
+        <Card>
+          <CardTitle title="Card Title" />
+          <CardText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+          </CardText>
+        </Card>
+        </div>
       </div>
     );
   }
