dc90b8344400e57b8f12d9f3543f7b8b28e040f5
Fadee Kannah <fadeekannah@gmail.com>
2017-07-30 00:37:26 -0700

    Finished header effect using particle js

--- src/components/visualEffect/objects/index.js
+++
@@ -1,7 +0,0 @@
-import pentagon from './pentagon.svg';
-import rhombus from './rhombus.svg';
-import x from './x.svg';
-import circle from './circle.svg';
-import point from './point.svg';
-
-export default [pentagon, rhombus, x, circle, point];
--- src/pages/homepage/header.js
+++ src/pages/homepage/header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router';
 import muiThemeable from 'material-ui/styles/muiThemeable';
-import AntiGravity from '../../components/visualEffect/index';
+import ParticlesBackground from '../../components/visualEffect/index';
 
 import logo from './logo.svg';
 
@@ -9,14 +9,15 @@ const styles = {
   textDecoration: 'none',
 };
 
+const height = "350px";
 const Header = (props) => (
   <Link to="/foo" style={styles}>
-    <div className="App-header" >
-      <AntiGravity height="390px"/>
+    <div className="App-header" style={{zIndex: 10, backgroundColor: '#222', height,  color: 'white'}}>
+      <ParticlesBackground style={{zIndex: 0, position: 'absolute', left: 0}} height={height}/>
       <img src={logo} className="App-logo" alt="logo" />
       <h2>Welcome to React</h2>
     </div>
-  </Link>
+  </Link> 
 );
 
 export default muiThemeable()(Header);
