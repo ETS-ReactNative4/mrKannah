263ad86c170e29c7c534565f19480185d2da6d13
Fadee kannah <fadeekannah@gmail.com>
2017-10-16 22:13:07 -0700

    Added typing for the header

--- src/pages/homepage/header.js
+++ src/pages/homepage/header.js
@@ -3,9 +3,18 @@ import { connect } from 'react-redux';
 import muiThemeable from 'material-ui/styles/muiThemeable';
 import ParticlesBackground from '../../components/visualEffect/index';
 import ProfilePicture from '../../components/misc/profilePicture';
+import Typist from 'react-typist';
 
 
 class Header extends Component {
+  state = {typing: true};
+  
+  restartTyping = () => {
+    this.setState({ typing: false }, () => {
+      setTimeout(() => this.setState({ typing: true }), 0);
+    });
+  };
+
   render() {
     let size = this.props.mobileView ? 100 : 200;
     let height = this.props.mobileView ? '200px' : '350px';
@@ -24,15 +33,25 @@ class Header extends Component {
           <div id="TextHolder" style={{display: 'table-cell', 
             verticalAlign: 'middle',
             position: 'relative',
-            fontSize: this.props.mobileView ? '0.8em' : '1em',
+            fontSize: this.props.mobileView ? '1em' : '1.2em',
             transition: 'all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
           }}>
-            <h1 style={{fontSize: this.props.mobileView ? '1.5em' : '2em'}}>What am I doing...??</h1>
-            <p>Too many thing:</p>
-            <ul>
-              <li>CourseKey's CTO by day</li>
-              <li>Developer & gamer by night</li>
-            </ul>
+            <h1 style={{fontSize: this.props.mobileView ? '1.5em' : '2em'}}>Hi, I'm Fadee.</h1>
+            <p>
+              <span>I engineer and build </span>
+              {this.state.typing ? 
+                <Typist onTypingDone={this.restartTyping}>
+                  <span>Software</span>
+                  <Typist.Backspace count={8} delay={1000}/>
+                  <span>Systems</span>
+                  <Typist.Backspace count={7} delay={1000}/>
+                  <span>Teams</span>
+                  <Typist.Backspace count={5} delay={1000}/>
+                </Typist> 
+                :
+                <span> </span>
+              }
+            </p>
           </div>
         </div>
       </div>
