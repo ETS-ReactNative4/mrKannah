e6afa0483b5eb611d9d347ec07d53331d7178bd8
Fadee kannah <fadeekannah@gmail.com>
2017-10-22 13:35:27 -0700

    Steps 1 and 2 ready

--- src/pages/contact/form.js
+++ src/pages/contact/form.js
@@ -14,6 +14,7 @@ class form extends Component {
     this.state = {
       step: 0,
       formData: {
+        name: '',
         email: '',
       },
       submitted: false,
@@ -53,8 +54,7 @@ class form extends Component {
     }
     this.setState({ step }, () => this.refs.form.walk(this.refs.form.childs));
   }
-
-
+  
   handleSubmit() {
     this.setState({ submitted: true }, () => {
       setTimeout(() => this.setState({ submitted: false }), 5000);
@@ -70,28 +70,41 @@ class form extends Component {
         content = (
           <Card>
             <CardText>
-              I think you'll agree, there's nothing better than a message from a complete stranger. It's an opportunity to get to know someone and make a new friend.<br />
-              If you're interested in me and what I do, get in touch.
+              What's your name, stranger?<br />
+              Mine's Fadee. Let's not be strangers for much longer.
             </CardText>
-            <RaisedButton
-              label={'Shoot me a message'}
-              onClick={this.nextStep}
-              secondary
+            <TextValidator
+              ref="name"
+              floatingLabelText="Your Name"
+              onBlur={this.handleBlur}
+              onChange={this.handleChange}
+              name="name"
+              value={formData.name}
+              validators={['required']}
+              errorMessages={['this field is required']}
             />
           </Card>
-        )
-        content = ( <div><TextValidator
-          ref="email"
-          floatingLabelText="Email"
-          onBlur={this.handleBlur}
-          onChange={this.handleChange}
-          name="email"
-          value={formData.email}
-          validators={['required', 'isEmail']}
-          errorMessages={['this field is required', 'email is not valid']}
-        />
-          <br/>
-        </div>)
+        );
+        break;
+      case 2:
+        content = (
+          <Card>
+            <CardText>
+              What's the best email address for you, {formData.name}?<br />
+              I'll use this to get back to you. No spam or unexpected newsletters here.
+            </CardText>
+            <TextValidator
+              ref="email"
+              floatingLabelText="Your Email"
+              onBlur={this.handleBlur}
+              onChange={this.handleChange}
+              name="email"
+              value={formData.email}
+              validators={['required', 'isEmail']}
+              errorMessages={['this field is required', 'please provide a valid email']}
+            />
+          </Card>
+        );
         break;
       default:
         content = (
@@ -110,7 +123,7 @@ class form extends Component {
           {content}
           <Stepper activeStep={step}>
             <Step>
-              <StepLabel>Introduce your self</StepLabel>
+              <StepLabel>Introduce yourself</StepLabel>
             </Step>
             <Step>
               <StepLabel>Create an ad group</StepLabel>
