97f3d619de169c4bc945e24b4719254d10d59c06
Fadee kannah <fadeekannah@gmail.com>
2018-10-20 22:19:22 -0700

    Updates

---
+++ precache-manifest.9ca9f1866c1d4c661633b5a616098d34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "224b5728e4516eac552553c56b7b3e12",
+    "url": "/static/media/logo.224b5728.svg"
+  },
+  {
+    "revision": "4973f52d7bba2fbdafdfacc5d4b74e3b",
+    "url": "/static/media/fadee.4973f52d.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f5f65122566c087078f6",
+    "url": "/static/js/main.f5f65122.chunk.js"
+  },
+  {
+    "revision": "1fda5c48562e9c9961ef",
+    "url": "/static/js/1.1fda5c48.chunk.js"
+  },
+  {
+    "revision": "f5f65122566c087078f6",
+    "url": "/static/css/main.28348eba.chunk.css"
+  },
+  {
+    "revision": "8c0c7684b3f2ff5ebe0cee82ac5a544c",
+    "url": "/index.html"
+  }
+];
