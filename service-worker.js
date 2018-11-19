6fce773b18ccc302d1fe9f33406094fd82d86ec7
Fadee kannah <fadeekannah@gmail.com>
2018-11-18 17:49:12 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.de2b291e6833f75c61de55e9f05c32d3.js"
+  "/precache-manifest.4025d3983262062e4129d1231342cc2e.js"
 );
 
 workbox.clientsClaim();
