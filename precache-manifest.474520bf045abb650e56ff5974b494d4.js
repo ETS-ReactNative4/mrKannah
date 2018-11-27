bd8634d55e4c4527ad6b98b46301a50644bd434c
Fadee kannah <fadeekannah@gmail.com>
2018-11-26 22:32:07 -0800

    Updates

---
+++ precache-manifest.474520bf045abb650e56ff5974b494d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "224b5728e4516eac552553c56b7b3e12",
+    "url": "/static/media/logo.224b5728.svg"
+  },
+  {
+    "revision": "4973f52d7bba2fbdafdfacc5d4b74e3b",
+    "url": "/static/media/fadee.4973f52d.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "82cfceddf8e2e42dbd36",
+    "url": "/static/js/main.82cfcedd.chunk.js"
+  },
+  {
+    "revision": "1e817849fa1709288747",
+    "url": "/static/js/1.1e817849.chunk.js"
+  },
+  {
+    "revision": "82cfceddf8e2e42dbd36",
+    "url": "/static/css/main.7e89e889.chunk.css"
+  },
+  {
+    "revision": "73670ca31f9ccf4c95cac30ce13843d5",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7b1e5f8f85cde8a662f754ee98beab65.js"
+  "/precache-manifest.474520bf045abb650e56ff5974b494d4.js"
 );
 
 workbox.clientsClaim();
